3f98832ce0cc2aadf5ddfa78c7a744d220528623
Nirbhay Yadav <nirbhaykumary100@gmail.com>
2026-03-26 11:51:48 +0530

    Added new folder

---
+++ getElementsByClassName Method/index.js
@@ -0,0 +1,7 @@
+// Write your code below:
+let items = document.getElementsByTagName("li");
+items[2].style.backgroundColor = "yellow";
+
+for (let i = 0; i < items.length; i++){
+    items[i].style.fontWeight = 'bold'
+}
